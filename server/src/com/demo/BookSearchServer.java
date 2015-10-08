e0e3838cd1b4131e706cae003aad5aee75edafa6
sfaria <scott.faria@gmail.com>
2015-10-08 09:25:53 -0400

    Added stubbed spark server.

---
+++ server/src/com/demo/BookSearchServer.java
@@ -0,0 +1,22 @@
+package com.demo;
+
+import static spark.Spark.externalStaticFileLocation;
+import static spark.Spark.get;
+import static spark.Spark.port;
+
+/**
+ * @author Scott Faria
+ */
+public final class BookSearchServer {
+
+    // -------------------- Main --------------------
+
+    public static void main(String[] args) {
+        port(9090);
+        externalStaticFileLocation("client/libs");
+
+        get("/", (request, response) -> {
+            return "Hello, World!";
+        });
+    }
+}
